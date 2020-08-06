c758913bcbdba7ceb91944387284ad9e494448f5
ADAM YE <adamye48@gmail.com>
2020-08-06 21:02:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(int)
+  if int % 3 == 0
+   puts "Fizz"
+  
+  if int % 5 == 0 
+   puts "Buzz"
+  
+   if int % 3 & 5 == 0 
+     puts "Fizzbuzz"
+   
+   if int % 3 & 5 != 0 
+    nil
+  end
+end
