dfef550a1969e83dda94d89eb012c1dffc95756a
ADAM YE <adamye48@gmail.com>
2020-08-06 21:02:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,15 @@
 def fizzbuzz(int)
   if int % 3 == 0
    puts "Fizz"
+  end
   
   if int % 5 == 0 
    puts "Buzz"
+  end
   
    if int % 3 & 5 == 0 
      puts "Fizzbuzz"
+   end
    
    if int % 3 & 5 != 0 
     nil
